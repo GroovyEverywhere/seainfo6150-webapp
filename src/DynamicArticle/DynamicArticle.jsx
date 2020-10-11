ac86b904624db020398b34e9decf37bb7bd9b915
GroovyEverywhere <leitanbruno@gmail.com>
2020-10-11 15:24:54 +0800

    [javascript assignment]

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -5,7 +5,11 @@ const DynamicArticle = (props) => {
   return (
     <article>
       {/* this is an example of how you insert props into a react component */}
-      <div>{props.article.title}</div>
+      <h1>{props.article.title}</h1>
+      <address>by {props.article.author}(
+        <a href = "malito:{props.article.authorEmail}">{props.article.authorEmail}</a> ) 
+      </address>
+      <time datetime = "{props.article.timeStamp}"> {props.article.displayDate}</time>
       <HTMLText text={props.article.text} />
     </article>
   );
