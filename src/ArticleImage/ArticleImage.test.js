3fedec4aefbd2bd1bc52686118a6729079a3ef4a
GroovyEverywhere <leitanbruno@gmail.com>
2020-11-22 11:46:43 +0800

    [Image Assignment]

---
+++ src/ArticleImage/ArticleImage.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ArticleImage from "./ArticleImage";
+
+describe("ArticleImage tests", () => {
+    it("renders correctly", () => {
+      const { container } = render(<ArticleImage/>);
+      expect(container).toMatchSnapshot();
+    });
+  });
+  
